5266411db554b97d3289b7abab91b47e547b391a
Emanuele <ele146cc@gmail.com>
2017-12-13 12:11:09 +0100

    Updated Junit version Configured persistence.xml for InMemoryDatabase

---
+++ src/test/java/it/unisa/libra/model/jpa/AziendaJpaTests.java
@@ -0,0 +1,14 @@
+package it.unisa.libra.model.jpa;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AziendaJpaTests {
+	
+	@Test
+	public void provola() {
+		assertTrue(false);
+	}
+	
+}
---
+++ src/test/java/it/unisa/libra/model/jpa/GenericJpaTests.java
@@ -0,0 +1,34 @@
+package it.unisa.libra.model.jpa;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GenericJpaTests {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testPersist() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRemove() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testFindById() {
+		fail("Not yet implemented");
+	}
+	
+}

eb90ffe30b77769e173e971f3ffe300b6cd05df3
Mavit <vitalemauro@outlook.it>
2017-12-15 23:54:45 +0100

    Configurazione pom.xml

--- src/main/java/it/unisa/libra/controller/RecuperoPasswordServlet.java
+++ src/main/java/it/unisa/libra/controller/RecuperoPasswordServlet.java
@@ -25,7 +25,6 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-
 /** Servlet implementation class AutenticazioneServlet */
 @WebServlet(name = "RecuperoPasswordServlet", urlPatterns = "/recupero")
 public class RecuperoPasswordServlet extends HttpServlet 

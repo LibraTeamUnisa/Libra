26f9436069f29b4cfe532f0d59fda2f62cdca0e9
Emanuele Rinaldi <ele146cc@gmail.com>
2017-12-17 18:35:43 +0100

    Junit for JsonUtils
    Updated AziendaJpaTest

--- src/test/java/it/unisa/libra/model/jpa/AziendaJpaTest.java
+++ src/test/java/it/unisa/libra/model/jpa/AziendaJpaTest.java
@@ -2,38 +2,39 @@ package it.unisa.libra.model.jpa;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import it.unisa.libra.bean.Azienda;
 import it.unisa.libra.bean.Utente;
 
 public class AziendaJpaTest extends GenericJpaTest {
-
-  private static UtenteJpa utenteJpa;
-
-  @BeforeClass
-  public static void setUp() {
-    utenteJpa = new UtenteJpa();
-    utenteJpa.entityManager = em;
-  }
-
-  @Test
-  public void persistTest() {
-
-    Utente utAzienda = new Utente();
-    utAzienda.setEmail("azienda@email.it");
-
-    Azienda toPersist = new Azienda();
-    toPersist.setUtenteEmail("azienda@email.it");
-    toPersist.setNome("RagioneSociale");
-
-    utAzienda.setAzienda(toPersist);
-
-    utenteJpa.persist(utAzienda);
-
-    Azienda toCheck = utenteJpa.findAll(Utente.class).get(0).getAzienda();
-
-    assertNotNull(toCheck);
-    assertEquals(toPersist.getNome(), toCheck.getNome());
-  }
+	
+	private static AziendaJpa aziendaJpa;
+	
+	@BeforeClass
+	public static void setUp() {
+		aziendaJpa = new AziendaJpa();
+		aziendaJpa.entityManager = em;
+	}
+	
+	@Test
+	public void persistTest() {
+		
+		Utente utAzienda = new Utente();
+		utAzienda.setEmail("azienda@email.it");
+		
+		Azienda toPersist = new Azienda();
+		toPersist.setUtenteEmail("azienda@email.it");
+		toPersist.setNome("RagioneSociale");
+		toPersist.setUtente(utAzienda);
+		
+		aziendaJpa.persist(toPersist);
+		
+		Azienda toCheck = aziendaJpa.findAll(Azienda.class).get(0);
+		
+		assertNotNull(toCheck);
+		assertEquals(toPersist.getNome(), toCheck.getNome());
+	}
 }

d8d9388e7d882af1b7f7574416cac813c3421750
Mario Ruggiero <mario1995@live.it>
2017-12-19 09:33:35 +0100

    insert buisness methods in DAO and JPA classes

--- src/main/java/it/unisa/libra/model/dao/IAziendaDao.java
+++ src/main/java/it/unisa/libra/model/dao/IAziendaDao.java
@@ -1,6 +1,12 @@
 package it.unisa.libra.model.dao;
 
+
+
+import java.util.List;
+
 import it.unisa.libra.bean.Azienda;
 
 public interface IAziendaDao extends IGenericDao<Azienda, String> {
+	
+	List<Azienda> findAll();
 }
--- src/main/java/it/unisa/libra/model/jpa/AziendaJpa.java
+++ src/main/java/it/unisa/libra/model/jpa/AziendaJpa.java
@@ -1,9 +1,16 @@
 package it.unisa.libra.model.jpa;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import it.unisa.libra.bean.Azienda;
 import it.unisa.libra.model.dao.IAziendaDao;
 
 @Stateless
 public class AziendaJpa extends GenericJpa<Azienda, String> implements IAziendaDao {
+
+	@Override
+	public List<Azienda> findAll() {
+		return entityManager.createNamedQuery("Azienda.findAll", Azienda.class).getResultList();
+	}
 }

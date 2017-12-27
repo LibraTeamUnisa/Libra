6a19a639b5fe22c93ed99608c568a507d3ae90ab
aLorenzo1 <andrea999@live.it>
2017-12-27 15:50:41 +0100

    Implementazione JSP e Servlet richiestaPF

--- src/main/java/it/unisa/libra/controller/RichiediPfServlet.java
+++ src/main/java/it/unisa/libra/controller/RichiediPfServlet.java
@@ -1,13 +1,39 @@
 package it.unisa.libra.controller;
 
 import java.io.IOException;
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import it.unisa.libra.bean.Azienda;
+import it.unisa.libra.bean.ProgettoFormativo;
+import it.unisa.libra.bean.Studente;
+import it.unisa.libra.bean.Utente;
+import it.unisa.libra.model.dao.IUtenteDao;
+import it.unisa.libra.model.dao.IAziendaDao;
+import it.unisa.libra.model.dao.IProgettoFormativoDao;
+import it.unisa.libra.model.dao.IStudenteDao;
 
-/** Servlet implementation class AutenticazioneServlet */
+
+
+/**
+ * Consente di effettuare una richiesta di progetto formativo ad un utente registrato con ruolo
+ * studente
+ */
+@WebServlet("/RichiediPfServlet")
 public class RichiediPfServlet extends HttpServlet {
+
+  @EJB
+  private IUtenteDao utenteDao;
+  @EJB
+  private IAziendaDao aziendaDao;
+  @EJB
+  private IProgettoFormativoDao pfDao;
+  @EJB
+  private IStudenteDao studenteDao;
 
   private static final long serialVersionUID = 1L;
 
@@ -20,9 +46,45 @@ public class RichiediPfServlet extends HttpServlet {
     response.getWriter().append("Served at: ").append(request.getContextPath());
   }
 
-  /** @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response) */
+  /** Gestisce le richieste di progetto formativo degli studenti */
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
-    doGet(request, response);
+
+    HttpSession session = request.getSession();
+
+    if (session.getAttribute("utenteEmail") != null) {
+      String email = (String) session.getAttribute("utenteEmail");
+      String aziendaEmail = request.getParameter("aziendaName");
+      Utente utente = utenteDao.findById(Utente.class, email);
+
+      if (utente != null) {
+        Azienda azienda = aziendaDao.findByName(aziendaEmail);
+        Studente studente = studenteDao.findById(Studente.class, email);
+
+        String defaultString = "/";
+        int deafuletInt = 0;
+
+        ProgettoFormativo pf = new ProgettoFormativo();
+        pf.setAzienda(azienda);
+        pf.setStudente(studente);
+
+        // Valori di default per i campi not null
+        pf.setTutorInterno(null);
+        pf.setAmbito(defaultString);
+        pf.setDocumento(defaultString);
+        pf.setStato(deafuletInt);
+        pf.setPeriodoReport(deafuletInt);
+
+        pfDao.persist(pf);
+
+        response.sendRedirect(
+            request.getContextPath() + "/profiloAziendale.jsp?nome=" + azienda.getNome() + "&stato=success");
+      } else {
+        response.getWriter().write("false");
+      }
+
+    } else {
+      response.getWriter().write("false");
+    }
   }
 }
